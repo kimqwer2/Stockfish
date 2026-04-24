092cf5c0e1009d91b6c462f32d25e8c064b75380
kimqwer2 <63884391+kimqwer2@users.noreply.github.com>
2026-04-24 17:58:58 +0900

    Port additional modern Stockfish search heuristics

--- fairy-stockfish/src/search.cpp
+++ fairy-stockfish/src/search.cpp
@@ -17,6 +17,7 @@
 */
 
 #include <algorithm>
+#include <array>
 #include <cassert>
 #include <cmath>
 #include <cstring>   // For std::memset
@@ -60,6 +61,9 @@ using namespace Search;
 
 namespace {
 
+  constexpr std::array<int, 16> lmrDivisor = {3307, 2930, 2874, 2818, 3215, 3225, 3224, 2782,
+                                               2858, 2919, 3088, 3275, 3180, 2868, 3006, 3599};
+
   // Different node types, used as a template parameter
   enum NodeType { NonPV, PV, Root };
 
@@ -81,6 +85,15 @@ namespace {
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
     return d > 14 ? 73 : 6 * d * d + 229 * d - 215;
+  }
+
+  bool is_shuffling(Move move, Stack* const ss, const Position& pos) {
+    if (pos.capture_or_promotion(move) || pos.rule50_count() < 11)
+        return false;
+    if (pos.state()->pliesFromNull <= 6 || ss->ply < 18)
+        return false;
+    return from_sq(move) == to_sq((ss - 2)->currentMove)
+        && from_sq((ss - 2)->currentMove) == to_sq((ss - 4)->currentMove);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
@@ -923,6 +936,14 @@ namespace {
     if (pos.must_capture() && pos.has_capture())
         goto moves_loop;
 
+    // Step 7. Razoring
+    if (   !PvNode
+        && !ss->ttPv
+        &&  eval < alpha - Value(486 + 298 * depth * depth)
+        &&  !pos.check_counting()
+        &&  !pos.must_capture())
+        return qsearch<NonPV>(pos, ss, alpha, beta);
+
     // Step 7. Futility pruning: child node (~50 Elo)
     if (   !PvNode
         &&  depth < 11 - 3 * pos.blast_on_capture()
@@ -991,6 +1012,8 @@ namespace {
                 return nullValue;
         }
     }
+
+    improving |= ss->staticEval >= beta;
 
     probCutBeta = beta + (209 + 20 * !!pos.flag_region(~pos.side_to_move()) + 50 * pos.captures_to_hand()) * (1 + pos.check_counting() + pos.extinction_single_piece()) - 44 * improving;
 
@@ -1165,18 +1188,37 @@ moves_loop: // When in check, search starts from here
           if (   captureOrPromotion
               || givesCheck)
           {
+              const int captHist = captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))];
+
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
                   && lmrDepth < 1
-                  && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
+                  && captHist < 0)
                   continue;
 
+              // Futility pruning for captures
+              if (!givesCheck && lmrDepth < 7)
+              {
+                  Value futilityValue = ss->staticEval
+                                      + Value(218 + 223 * lmrDepth + 131 * captHist / 1024)
+                                      + PieceValue[EG][pos.piece_on(to_sq(move))];
+                  if (futilityValue <= alpha)
+                      continue;
+              }
+
               // SEE based pruning
-              if (!pos.see_ge(move, Value(-218 - 120 * pos.captures_to_hand()) * depth)) // (~25 Elo)
+              if (!pos.see_ge(move, Value(-std::max(167 * depth + 34 * captHist / 1024, 0)))) // (~25 Elo)
                   continue;
           }
           else
           {
+              int dIndex = std::min(int(depth), int(lmrDivisor.size())) - 1;
+              int history = (*contHist[0])[history_slot(movedPiece)][to_sq(move)]
+                          + (*contHist[1])[history_slot(movedPiece)][to_sq(move)];
+
+              history += 71 * thisThread->mainHistory[us][from_to(move)] / 32;
+              lmrDepth += history / lmrDivisor[dIndex];
+
               // Continuation history based pruning (~20 Elo)
               if (   lmrDepth < 5
                   && (*contHist[0])[history_slot(movedPiece)][to_sq(move)] < CounterMovePruneThreshold
@@ -1187,7 +1229,7 @@ moves_loop: // When in check, search starts from here
               if (   lmrDepth < 7
                   && !ss->inCheck
                   && !pos.extinction_single_piece()
-                  && ss->staticEval + (174 + 157 * lmrDepth) * (1 + pos.check_counting()) <= alpha
+                  && ss->staticEval + (42 + 151 * !bestMove + 120 * lmrDepth + 86 * (ss->staticEval > alpha)) * (1 + pos.check_counting()) <= alpha
                   &&  (*contHist[0])[history_slot(movedPiece)][to_sq(move)]
                     + (*contHist[1])[history_slot(movedPiece)][to_sq(move)]
                     + (*contHist[3])[history_slot(movedPiece)][to_sq(move)]
@@ -1214,9 +1256,10 @@ moves_loop: // When in check, search starts from here
        /* &&  ttValue != VALUE_NONE Already implicit in the next condition */
           &&  abs(ttValue) < VALUE_KNOWN_WIN
           && (tte->bound() & BOUND_LOWER)
-          &&  tte->depth() >= depth - 3)
+          &&  tte->depth() >= depth - 3
+          && !is_shuffling(move, ss, pos))
       {
-          Value singularBeta = ttValue - 2 * depth;
+          Value singularBeta = ttValue - Value((60 + 66 * (ss->ttPv && !PvNode)) * depth / 55);
           Depth singularDepth = (depth - 1) / 2;
 
           ss->excludedMove = move;
@@ -1225,12 +1268,14 @@ moves_loop: // When in check, search starts from here
 
           if (value < singularBeta)
           {
-              extension = 1;
+              int doubleMargin = 53 - 180 * !ttCapture + 180 * PvNode - 130 * (ss->ply > thisThread->rootDepth);
+              int tripleMargin = 145 - 220 * !ttCapture + 260 * PvNode - 150 * (ss->ply > thisThread->rootDepth);
+              extension = 1 + (value < singularBeta - doubleMargin);
               singularQuietLMR = !ttCapture;
 
               // Avoid search explosion by limiting the number of double extensions to at most 3
               if (   !PvNode
-                  && value < singularBeta - 93
+                  && value < singularBeta - tripleMargin
                   && ss->doubleExtensions < 3)
               {
                   extension = 2;
@@ -1257,6 +1302,12 @@ moves_loop: // When in check, search starts from here
               if (value >= beta)
                   return beta;
           }
+
+          // Negative extensions when singularity cannot be confirmed
+          else if (ttValue >= beta)
+              extension = -2 - cutNode;
+          else if (cutNode)
+              extension = -1;
       }
       else if (   givesCheck
                && depth > 6
@@ -1345,6 +1396,8 @@ moves_loop: // When in check, search starts from here
               if (!ss->inCheck)
                   r -= ss->statScore / (14721 - 4434 * pos.captures_to_hand());
           }
+          else if (ttMove == MOVE_NONE)
+              r += 1;
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
@@ -1355,6 +1408,12 @@ moves_loop: // When in check, search starts from here
 
           // If the son is reduced and fails high it will be re-searched at full depth
           doFullDepthSearch = value > alpha && d < newDepth;
+          if (doFullDepthSearch)
+          {
+              const bool doDeeperSearch = d < newDepth && value > bestValue + 45;
+              const bool doShallowerSearch = value < bestValue + 10;
+              newDepth += doDeeperSearch - doShallowerSearch;
+          }
           didLMR = true;
       }
       else
@@ -1371,6 +1430,8 @@ moves_loop: // When in check, search starts from here
           // If the move passed LMR update its stats
           if (didLMR && !captureOrPromotion)
           {
+              if (value > alpha)
+                  update_continuation_histories(ss, movedPiece, to_sq(move), 1365);
               int bonus = value > alpha ?  stat_bonus(newDepth)
                                         : -stat_bonus(newDepth);
 
